623a0b97aa77c3fc00d7909924846c2cbf2a3175
dyrnq <dyrnq@qq.com>
2024-09-20 21:03:04 +0800

    Support tls clent

--- src/main/java/com/dyrnq/GrpcExampleClient.java
+++ src/main/java/com/dyrnq/GrpcExampleClient.java
@@ -2,10 +2,19 @@ package com.dyrnq;
 
 import com.dyrnq.grpc.BiDirectionalExampleService;
 import com.dyrnq.grpc.StreamServiceGrpc;
-import io.grpc.*;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NegotiationType;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.ClientAuth;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
 import io.grpc.stub.StreamObserver;
+import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine;
 
+import java.io.File;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
@@ -18,10 +27,42 @@ public class GrpcExampleClient implements Callable<Integer> {
     @CommandLine.Option(names = {"-s", "--server"}, description = "server",defaultValue = "127.0.0.1:50053")
     String server;
 
+    @CommandLine.Option(names = {"--ca"}, description = "ca")
+    String ca;
+
+    @CommandLine.Option(names = {"--cert"}, description = "cert")
+    String cert;
+    @CommandLine.Option(names = {"--key"}, description = "key")
+    String key;
+
     @Override
     public Integer call() throws Exception {
         //ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50053).usePlaintext().build();
-        ManagedChannel channel = Grpc.newChannelBuilder(server, InsecureChannelCredentials.create()).build();
+
+        ManagedChannel channel;
+        // 创建 gRPC 通道
+        if (StringUtils.isNoneBlank(ca) && StringUtils.isNoneBlank(cert) && StringUtils.isNoneBlank(key)) {
+            // 客户端证书和私钥
+            File clientCert = new File(cert);
+            File clientKey = new File(key);
+            // CA 证书
+            File trustCertificate = new File(ca);
+            // 创建 SSL 上下文
+            SslContext sslContext = GrpcSslContexts
+                    .forClient()
+                    .clientAuth(ClientAuth.REQUIRE)
+                    .trustManager(trustCertificate) // 设置信任管理器
+                    .keyManager(clientCert, clientKey)
+                    .build();
+            channel = NettyChannelBuilder.forTarget(server)
+                    .negotiationType(NegotiationType.TLS)
+                    .sslContext(sslContext)
+                    .build();
+        } else {
+            channel = Grpc.newChannelBuilder(server, InsecureChannelCredentials.create()).build();
+        }
+
+
         StreamServiceGrpc.StreamServiceStub service = StreamServiceGrpc.newStub(channel);
         AtomicReference<StreamObserver<BiDirectionalExampleService.Request>> requestObserverRef = new AtomicReference<>();
 
