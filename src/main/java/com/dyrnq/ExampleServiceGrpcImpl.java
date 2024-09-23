4492550f85b16cf11cecdcc170ae2c58e70cafbc
dyrnq <dyrnq@qq.com>
2024-09-23 08:35:43 +0800

    Add initiative server call client

--- src/main/java/com/dyrnq/ExampleServiceGrpcImpl.java
+++ src/main/java/com/dyrnq/ExampleServiceGrpcImpl.java
@@ -10,14 +10,33 @@ public class ExampleServiceGrpcImpl extends StreamServiceGrpc.StreamServiceImplB
     @Override
     public StreamObserver<BiDirectionalExampleService.Request> channel(StreamObserver<BiDirectionalExampleService.Response> responseObserver) {
 
-        StreamObserver<BiDirectionalExampleService.Request> so = new StreamObserver<BiDirectionalExampleService.Request>() {
-            @Override
-            public void onNext(BiDirectionalExampleService.Request value) {
+        // 在这里启动一个新线程,定期向客户端发送消息
+        new Thread(() -> {
+            while (true) {
                 try {
-                    Thread.sleep(1000L);
+                    Thread.sleep(2000); // 每2秒发送一次
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
+
+                // 构建要发送的响应消息
+                BiDirectionalExampleService.Response response = BiDirectionalExampleService.Response.newBuilder()
+                        .setTotal(99999999)
+                        .build();
+
+                // 发送消息给客户端
+                responseObserver.onNext(response);
+            }
+        }).start();
+
+        StreamObserver<BiDirectionalExampleService.Request> so = new StreamObserver<BiDirectionalExampleService.Request>() {
+            @Override
+            public void onNext(BiDirectionalExampleService.Request value) {
+//                try {
+//                    Thread.sleep(1000L);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                }
                 total++;
                 System.out.println("Connecting stream observer " + total + ", " + value.getName());
                 responseObserver.onNext(BiDirectionalExampleService.Response.getDefaultInstance().toBuilder().setTotal(total).build());
